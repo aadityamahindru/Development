10133cd4a30a301b78dc04aa1b9e64198eb8ffe4
aadityamahindru <aaditya2069@gmail.com>
2020-07-02 20:20:53 +0530

    excel clone

--- excel_clone/index.js
+++ excel_clone/index.js
@@ -5,7 +5,6 @@ $(document).ready(
             let rid=Number($(this).attr("row-id"));
             let cid=Number($(this).attr("col-id"));
             let ciAdrr = String.fromCharCode(cid + 65);
-            console.log(rid+" "+cid+" "+ciAdrr);
             $("#address-container").val(ciAdrr +(rid));
         })
     }
