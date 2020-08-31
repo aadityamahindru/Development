9fbdbec2b207a4bf5a87d8ba7a7e727cad8e81a4
aadityamahindru <aaditya2069@gmail.com>
2020-08-31 23:20:27 +0530

    added patch and delete

--- Instagram-Clone/raw/poc/crud/api.js
+++ Instagram-Clone/raw/poc/crud/api.js
@@ -41,7 +41,33 @@ app.patch("/api/users/:user_id",function(req,res){
             break;
         }
     }
-    
+    user.name=update.name;
+    user.user_id=update.user_id;
+    user.password=update.password;
+    user.email=update.email;
+    user.age=update.age;
+    fs.writeFileSync(path.join(__dirname,"user.json"),JSON.stringify(userDB));
+    res.status(200).json({
+        sucess:"sucessful",
+        user:user!=undefined?user:"not found"
+    })
+})
+app.delete("/api/users/:user_id",function(req,res){
+    let{user_id}=req.params;
+    let i;
+    let user;
+    for(i=0;i<userDB.length;i++){
+        if(userDB[i].user_id==user_id){
+            user=userDB[i];
+            break;
+        }
+    }
+    userDB.splice(i,1);
+    fs.writeFileSync(path.join(__dirname,"user.json"),JSON.stringify(userDB));
+    res.status(200).json({
+        sucess:"sucessful",
+        user:user!=undefined?user:"not found"
+    })
 })
 app.listen(3000, function () {
     console.log("Server is listening at port 3000");
