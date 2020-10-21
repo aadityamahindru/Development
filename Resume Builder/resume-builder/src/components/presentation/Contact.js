c847408a00442d8a5a0b7880074034344b1173e5
aadityamahindru <aaditya2069@gmail.com>
2020-10-21 18:54:02 +0530

    form

--- Resume Builder/resume-builder/src/components/presentation/Contact.js
+++ Resume Builder/resume-builder/src/components/presentation/Contact.js
@@ -1,11 +1,117 @@
-import React from 'react'
+import React, { Component } from 'react';
+import { NavLink } from 'react-router-dom';
+import Preview from './Preview';
+class Contact extends Component {
+    state = {}
+    render() {
+        return (
+            <div className='container mid contact full-height'>
+                <div className='section'>
+                    <div className='form-card'>
+                        <h2 className='form-heading center'>Personal Details</h2>
+                        <div className='form-section'>
+                            <div className='input-group'>
+                                <label>First Name</label>
+                                <div className='effect'>
+                                    <input type='text' name='FNAM' /><span></span>
+                                </div>
+                                <div className='error'></div>
+                            </div>
 
-function Contact() {
-    return (
-        <div>
-            
-        </div>
-    )
+                            <div className='input-group'>
+                                <label>Last Name</label>
+                                <div className='effect'>
+                                    <input type='text' name='LNAM' /><span></span>
+                                </div>
+                                <div className='error'></div>
+                            </div>
+
+                            <div className='input-group full'>
+                                <label>Professional Summary</label>
+                                <div className='effect'>
+                                    <input type='text' name='PRSU' /><span></span>
+                                </div>
+                                <div className='error'></div>
+                            </div>
+
+                            <div className='input-group'>
+                                <label>Email</label>
+                                <div className='effect'>
+                                    <input type='text' name='EMAI' /><span></span>
+                                </div>
+                                <div className='error'></div>
+                            </div>
+
+                            <div className='input-group'>
+                                <label>Phone</label>
+                                <div className='effect'>
+                                    <input type='text' name='PHON' /><span></span>
+                                </div>
+                                <div className='error'></div>
+                            </div>
+
+                            <div className='input-group'>
+                                <label>Profession</label>
+                                <div className='effect'>
+                                    <input type='text' name='PROF' /><span></span>
+                                </div>
+                                <div className='error'></div>
+                            </div>
+
+                            <div className='input-group'>
+                                <label>Street</label>
+                                <div className='effect'>
+                                    <input type='text' name='STRT' /><span></span>
+                                </div>
+                                <div className='error'></div>
+                            </div>
+
+                            <div className='input-group'>
+                                <label>City</label>
+                                <div className='effect'>
+                                    <input type='text' name='CITY' /><span></span>
+                                </div>
+                                <div className='error'></div>
+                            </div>
+
+                            <div className='input-group'>
+                                <label>State</label>
+                                <div className='effect'>
+                                    <input type='text' name='STAT' /><span></span>
+                                </div>
+                                <div className='error'></div>
+                            </div>
+
+                            <div className='input-group'>
+                                <label>Country</label>
+                                <div className='effect'>
+                                    <input type='text' name='CNTY' /><span></span>
+                                </div>
+                                <div className='error'></div>
+                            </div>
+
+                            <div className='input-group'>
+                                <label>Pin Code</label>
+                                <div className='effect'>
+                                    <input type='text' name='ZIPC' /><span></span>
+                                </div>
+                                <div className='error'></div>
+                            </div>
+
+                            <div className='form-buttons'>
+                                <button className='btn'>Next</button>
+                                <NavLink to='/education' className='center back-btn'>Back</NavLink>
+                            </div>
+
+                        </div>
+                    </div>
+                    <div className='preview-card'>
+                        <Preview></Preview>
+                    </div>
+                </div>
+            </div>
+        );
+    }
 }
 
-export default Contact
+export default Contact;
---
+++ Resume Builder/resume-builder/src/components/presentation/Preview.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function Preview() {
+    return (
+        <div className='resume-preview'>
+            
+        </div>
+    )
+}
+
+export default Preview
